49cb33bd920af1926766ddcb7b0ff2eca842920d
luiseurban <165191210+luiseurban@users.noreply.github.com>
2025-12-19 14:14:33 -0500

    Actualizar estilos Header y Footer

--- cryopath-frontend/src/components/layout/footer/Footer.jsx
+++ cryopath-frontend/src/components/layout/footer/Footer.jsx
@@ -1,11 +1,11 @@
-import "./Footer.css";
+import "./Footer.css"
 
 const Footer = () => {
   return (
     <footer className="footer">
-        <p>&copy; 2024 Cryopath. All rights reserved.</p>
+        <p>&copy; 2025 Cryopath. All rights reserved.</p>
     </footer>
   )
 }
 
-export default Footer;
+export default Footer
