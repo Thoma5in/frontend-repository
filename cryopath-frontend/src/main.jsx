df1adb2cb613beec0bac7d2c6bed335e6d83cbad
luiseurban <165191210+luiseurban@users.noreply.github.com>
2025-12-19 13:24:48 -0500

    Instalar dependencia React-Router

--- cryopath-frontend/src/main.jsx
+++ cryopath-frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Layout from './components/layout/Layout.jsx'
+import Profile from './pages/profile/Profile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/*" element={<App />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   </StrictMode>,
 )
