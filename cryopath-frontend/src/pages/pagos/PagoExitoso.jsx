bb54cf32d120ddc5e52644bb73a89fbb7b75759b
Samuel Ospina <samiospina2006@gmail.com>
2026-02-17 17:24:53 -0500

    Componente PagoExitoso

---
+++ cryopath-frontend/src/pages/pagos/PagoExitoso.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function PagoExitoso() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const token = params.get("token");
+
+        if(token) {
+            fetch (`http://localhost:3003/pagos/capturar/${token}`, {
+                method: "POST",
+            })
+            .then (async res => {
+                if (!res.ok) {
+                    const text = await res.text();
+                    throw new Error(text);
+                }
+                return res.json();
+                })
+            .then(data => {
+                console.log("CAPTURA", data);
+                navigate("/home")
+            })
+            .catch(err => console.error(err))
+        }
+    }, [])
+
+    return (
+        <h2>Procesando pago...</h2>
+    )
+}
+
+export default PagoExitoso;
